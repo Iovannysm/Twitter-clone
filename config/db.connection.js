bcac224d0224f430e78de22900d46781045c6aca
kathyswu <kathy.s.wu@hotmail.com>
2021-10-18 11:21:02 -0700

    Create database and connection

--- config/db.connection.js
+++ config/db.connection.js
@@ -1,4 +1,19 @@
 const mongoose = require("mongoose");
 
+// for env file
 require("dotenv").config();
 
+mongoose.connect(process.env.MONGODB_URI);
+
+// connection listeners
+mongoose.connection.on("connected", function () {
+  console.log(`[${new Date().toLocaleTimeString()}] - MongoDB connected`);
+});
+
+mongoose.connection.on("error", function () {
+  console.log(`[${new Date().toLocaleTimeString()}] - MongoDB error`);
+});
+
+mongoose.connection.on("disconnected", function () {
+  console.log(`[${new Date().toLocaleTimeString()}] - MongoDB disconnected`);
+});
