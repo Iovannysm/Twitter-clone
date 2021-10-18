f5048e4d8257dbe2e26b197f85fd9df5d8300c5c
Iovannysm <oenjad@gmail.com>
2021-10-18 11:20:09 -0700

    Created te logic for the tweet model and added a index file to the models to be able to export them.

---
+++ models/Tweet.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const tweetSchema = new mongoose.Schema({
+    
+    
+    content: {
+        type: String,
+        required: [true, "The field can not be empty."],
+      },
+      user: {
+        type: mongoose.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+  
+    
+}, 
+    {
+    timestamps: true,
+    }
+);
+
+const Tweet = mongoose.model("Tweet", tweetSchema);
+
+module.exports = Tweet;
