7951cfe2ba4d0f2669f73a7a686830301b98d2ab
Iovannysm <oenjad@gmail.com>
2021-10-21 10:07:44 -0700

    Createed the who page added the user session gllobal and display single tweet

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -5,35 +5,40 @@ const router = express.Router();
 const { Comment } = require("../models");
 
 // === Index
-router.get("/", function (req, res, next){
-    Comment.find({}, function(error, allComments) {
-        if (error) console.log(error);
-            const context = {
-            tweets: allComments,
-            };
-        return res.render("tweet/index", context);
-    });
-});
+// router.get("/", async function (req, res, next){
+
+//     Comment.find({}, function(error, allComments) {
+//         if (error) console.log(error);
+//             const context = {
+//             tweets: allComments,
+//             };
+//         return res.render("tweet/index", context);
+//     })
+// });
 
 
 // Show
-router.get("/:id", function (req, res, next) {
-    
+router.get("/:id", async function (req, res, next) {
 
-    Comment.findById(req.params.id, function (error, comment) {
-      if (error) {
-        req.error = error;
-        return next();
-      }
-  
-        const context = {
-          comment,
-          
-        };
-  
-        
-        return res.render("tweet/show", context);
-    });
+    try {
+    
+        const Tweet = await Tweet.findById(req.params.id).populate("user");
+        const allComments = await Comment.find({tweet: req.params.id}).populate("user").sort("-createdAt");
+        const allUsers = await User.find({});       
+              const context = {
+              tweets: Tweet,
+              comments: allComments,
+              users: allUsers,
+              
+              }
+              console.log(context);
+          return res.render("tweet/show", context);
+    } catch (error) {
+          console.log(error);
+          req.error = error;
+          next();
+    } 
+    
 });
 
 
